062adc53178ca0a9d7515feec09ad25eebfda441
Kevin Polin <140556459+KevPolin@users.noreply.github.com>
2023-07-31 14:38:30 -0400

    Add files via upload

---
+++ Staircase.cs
@@ -0,0 +1,30 @@
+﻿//HackerRank: Staircase
+
+//This is a staircase of size : 4
+
+//    #
+//   ##
+//  ###
+// ####
+
+//Its base and height are both equal to . It is drawn using # symbols and spaces.
+//The last line is not preceded by any spaces.
+//Write a program that prints a staircase of size .
+
+class Staircase
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Running project, Staircase, with parameters: 12");
+
+        staircase(12);
+
+        Console.WriteLine("Finished running project");
+    }
+
+    public static void staircase(int n)
+    {
+
+
+    }
+}
